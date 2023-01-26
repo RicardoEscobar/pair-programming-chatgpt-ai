005e716743d35b2defa43b39b2fd0b6662bda410
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-26 12:43:19 -0600

    Add new parameter `programming_language` in controller\generate_prompt.py

--- controller/generate_prompt.py
+++ controller/generate_prompt.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
 
-def generate_prompt(prompt: str, text_file_path: str | Path = None) -> str:
+def generate_prompt(
+        prompt: str,
+        text_file_path: str | Path = None,
+        programming_language: str = None) -> str:
+
     # Load source code file
     if text_file_path:
         with open(text_file_path, mode='r', encoding='utf-8') as file:
-            python_source_code = f"```python\n{file.read()}```"
+            python_source_code = f"```{programming_language.lower() if programming_language else ''}\n{file.read()}```"
     else:
         python_source_code = ''
 
-    return prompt + python_source_code
+    return f"{prompt}{python_source_code}"
