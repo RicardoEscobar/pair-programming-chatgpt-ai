da787d4f463febb8c119f05f9a70492c404c71c8
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2023-01-28 13:37:05 -0600

    Add model\read_or_create_file.py

---
+++ model/read_or_create_file.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Union
+
+
+def read_or_create_file(file_path: Union[str, Path], placeholder_text: str) -> str:
+    """
+    Reads the contents of a file at the provided file path. If the file does not exist, it is created with the provided
+    placeholder text as its contents.
+
+    :param file_path: The path to the file to be read or created.
+    :param placeholder_text: The text to be used as the contents of the file if it is created.
+    :return: The contents of the file.
+    """
+    with open(file_path, "a+", encoding='utf-8') as file:
+        file.seek(0)
+        contents = file.read()
+        if contents == "":
+            file.write(placeholder_text)
+            file.seek(0)
+            contents = file.read()
+    return contents
